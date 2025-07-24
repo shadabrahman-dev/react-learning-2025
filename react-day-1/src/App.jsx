b0655d707c1b7292f3bc43e72a94f3edc1a35804
shadabrahman-dev <shadabrahman.dpu@gmail.com>
2025-07-24 18:40:46 +0530

    readMe.txt added

--- react-day-1/src/App.jsx
+++ react-day-1/src/App.jsx
@@ -3,14 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-     <h1>hello React</h1>
-    </>
+function App()
+{
+  return(
+    <div>
+      <h1>hello world</h1>
+    </div>
   )
 }
 
-export default App
+export default App;
